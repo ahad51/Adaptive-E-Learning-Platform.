a453ebe8bd36d0ab4b0d8dd38005381d1de89920
ahad51 <abdul.ahad@zweidevs.com>
2023-09-24 23:05:15 +0500

    Login:Render Ui

---
+++ src/Components/Main Components/Login/Login.jsx
@@ -0,0 +1,79 @@
+import React,{useState} from "react";
+import { TextField } from "@mui/material";
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormControl from '@mui/material/FormControl';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import {Button} from "@mui/material";
+
+import './Login.css'
+
+const Login =()=>
+{
+    const [showPassword, setShowPassword] = React.useState(false);
+
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
+  
+    const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+      event.preventDefault();
+    };
+  return(
+
+    <>
+    <div className="background-container">
+        <div className="headingLogin">Adapt Learn</div>
+    <div className="mainContainer">
+        <div className="subMainContainerLogin">
+        </div>
+        <div className="submainContainer">
+        <h1 className="login">
+        Login to your Account
+        </h1>
+        <label htmlFor="email" className="field-label">
+          Email
+        </label>
+            <TextField placeholder="Enter you Email" className="textField" id="outlined-basic" variant="outlined" />
+            <label htmlFor="email" className="field-labelPassword">
+            Password
+        </label>
+            <FormControl sx={{ m: 1, width: '31ch' }} variant="outlined">
+          <OutlinedInput
+            id="outlined-adornment-password"
+            type={showPassword ? 'text' : 'password'}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                placeholder="Enter Your Password"
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                  className="passwordPlace"
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+          />
+        </FormControl>
+        <a className="linkForgot">Forgot Password?</a>
+        <div className="buttonContainer">
+        <Button className="authButton">Login</Button>
+        <div>
+        <h5 className="bottomHeadingLogin">
+        Don't have an account?  <a className="link">Signup Now!</a>
+        </h5>
+        </div>
+        </div>
+        </div>
+    </div>
+    </div>
+    </>
+  )
+
+}
+export default Login;

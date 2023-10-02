3a54af5101f3e2744803316cf227cdee38f05fc6
ahad51 <abdul.ahad@zweidevs.com>
2023-10-02 12:00:33 +0500

    AuthScreen:Change Logo on auth scrrens

--- src/Components/Main Components/Signup/Signup.jsx
+++ src/Components/Main Components/Signup/Signup.jsx
@@ -45,7 +45,6 @@ const Signup =()=>
         </div>
         <div className="submainContainer">
         <h1 className="login">
-          
         Create An Account
         </h1>
         <label htmlFor="email" className="field-label">

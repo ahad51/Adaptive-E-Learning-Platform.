bec89d7e720dcb66dcdf15c309c9e6d9b61a314c
Fmajid29 <faizanmajid11@gmail.com>
2024-07-05 19:32:19 +0500

    installed video library,Videos.jsx: added youtube video

--- src/Components/Main Components/AdvisorDashboard/Videoes.jsx
+++ src/Components/Main Components/AdvisorDashboard/Videoes.jsx
@@ -1,6 +1,7 @@
 
 import React from "react";
 import "./Video.css";
+import ReactPlayer from 'react-player/youtube';
 import video from "../../../assets/videoes/sample.mp4"
 import video2 from "../../../assets/videoes/sample2.mp4"
 
@@ -45,6 +46,11 @@ const Video = () => {
         />
         Your browser does not support the video tag.
       </video>
+      
+    </div>
+    <div className="yt-video">
+      <h1>My YouTube Video</h1>
+      <ReactPlayer url='https://www.youtube.com/watch?v=7inhRWxQMFk' />
     </div>
     </>
   );

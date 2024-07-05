92d81805f97d2668693d83f84cad41107a3c8e96
Fmajid29 <faizanmajid11@gmail.com>
2024-07-06 00:51:06 +0500

    Video.jsx: in student section video screen is okay now

--- src/Components/Main Components/Dashboard/Videoes.jsx
+++ src/Components/Main Components/Dashboard/Videoes.jsx
@@ -1,50 +1,48 @@
 
-import React from "react";
+import React, { useState } from "react";
 import "./Video.css";
-import video from "../../../assets/videoes/sample.mp4"
-import video2 from "../../../assets/videoes/sample2.mp4"
+import ReactPlayer from 'react-player/youtube';
 
 const Video = () => {
   return (
     <>
-    <div className="video-container">
-      <video controls>
-        <source
-        src={video} type="video/webm"
+    <div className="video-container">      
+      <div className="yt-video">
+        <ReactPlayer 
+        className="reactPlayer"
+        width='100%'
+        height="100%"
+        url='https://www.youtube.com/watch?v=W6NZfCO5SIk&t=2s&pp=ygUKamF2YXNjcmlwdA%3D%3D'
+        controls = {true}
         />
-        Your browser does not support the video tag.
-      </video> <video controls>
-        <source
-        src={video2} type="video/webm"
+      </div>
+      <div className="yt-video">
+        <ReactPlayer 
+        className="reactPlayer"
+        width='100%'
+        height="100%"
+        url='https://www.youtube.com/watch?v=lkIFF4maKMU&pp=ygUKamF2YXNjcmlwdA%3D%3D'
+        controls = {true}
         />
-        Your browser does not support the video tag.
-      </video>
-    </div>
-    <div className="video-container">
-      <video controls>
-        <source
-        src={video} type="video/webm"
+      </div>
+      <div className="yt-video">
+        <ReactPlayer 
+        className="reactPlayer"
+        width='100%'
+        height="100%"
+        url='https://www.youtube.com/watch?v=PkZNo7MFNFg&pp=ygUKamF2YXNjcmlwdA%3D%3D'
+        controls = {true}
         />
-        Your browser does not support the video tag.
-      </video> <video controls>
-        <source
-        src={video2} type="video/webm"
+      </div>
+      <div className="yt-video">
+        <ReactPlayer 
+        className="reactPlayer"
+        width='100%'
+        height="100%"
+        url='https://www.youtube.com/watch?v=E3XxeE7NF30&pp=ygUKamF2YXNjcmlwdA%3D%3D'
+        controls = {true}
         />
-        Your browser does not support the video tag.
-      </video>
-    </div>
-    <div className="video-container">
-      <video controls>
-        <source
-        src={video} type="video/webm"
-        />
-        Your browser does not support the video tag.
-      </video> <video controls>
-        <source
-        src={video2} type="video/webm"
-        />
-        Your browser does not support the video tag.
-      </video>
+      </div>
     </div>
     </>
   );
